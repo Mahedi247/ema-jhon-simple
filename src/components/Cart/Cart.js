83c25f490737cea7378675a3a207a27519750303
mahedi247 <mahedihasanug360@gmail.com>
2022-03-22 23:09:19 +0600

    taxex

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = ({cart}) => {
+const Cart = ({ cart }) => {
+    console.log(cart);
+    let total = 0;
+    let shipping = 0;
+    for (const product of cart) {
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+
+    }
+    const tax = (total*0.1).toFixed(2);
+
+
+
     return (
-        <div>
+        <div className='cart'>
             <h4>Order summary</h4>
             <p>Selected Item: {cart.length}</p>
+            <p>Total price: ${total}</p>
+            <p>Total Shipping: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <p><strong>Grand Total: $</strong></p>
         </div>
     );
 };
